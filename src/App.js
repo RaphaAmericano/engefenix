37181f7e25c0253de981306308dc2a7170bce3c3
RaphaAmericano <raphael@raphaelamericano.com.br>
2022-06-16 17:15:40 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,58 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import styled from 'styled-components';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../src/assets/logo_engefenix.jpg';
+
+const Img = styled.img`
+  width: 100%;
+` 
+const Anchor = styled.a`
+  color: #585858;
+  text-decoration: none;
+`
+
+const Column = styled(Col)`
+  display: flex;
+  justify-content: center;
+  font-family: 'Noto Sans', sans-serif;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  return <Container fluid gap={2} className="p-5">
+            <Row>
+              <Col>
+                <Img src={logo} />
+              </Col>
+            </Row>
+            <Row className="my-5">
+              <Column>
+                <Anchor href="mailto:contato@engefenix.com.br">contato@engefenix.com.br</Anchor>
+              </Column>
+            </Row>
+          </Container>;
 }
 
 export default App;
